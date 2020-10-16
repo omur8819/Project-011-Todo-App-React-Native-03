0ef08f84d5268f7bac63e20dcfd390db8bd8ae69
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-16 23:40:27 +0300

    project-update

--- todoApp/App.js
+++ todoApp/App.js
@@ -1,126 +1,138 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
 import {
+  View,
   StyleSheet,
   Text,
-  View,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
   FlatList,
-  Platform,
-  KeyboardAvoidingView,
-  ScrollView,
 } from 'react-native';
-import {Header, AddTodo, TodoItem} from './components';
-import Icon from 'react-native-vector-icons/dist/FontAwesome';
+
+import {ToDoCard} from './components';
 
 const App = () => {
-  const [todoList, setTodoList] = useState([
-    {
-      text: 'do homework',
-      done: false,
-      check: <Icon name="check" size={25} color="#819ca9" />,
-      del: <Icon name="trash" size={30} color="#819ca9" />,
-      key: '1',
-    },
-    {
-      text: 'recap js',
-      done: true,
-      check: <Icon name="check" size={25} color="#819ca9" />,
-      del: <Icon name="trash" size={30} color="#819ca9" />,
-      key: '2',
-    },
-  ]);
+  const [todo, setTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const [todoId, setId] = useState(0);
 
-  //Check isDone*******
-  const pressCheck = (key) => {
-    setTodoList((prevTodoList) => {
-      return prevTodoList.map((todo) =>
-        todo.key === key ? {...todo, done: !todo.done} : todo,
-      );
-    });
+  const addTodo = () => {
+    for (let item of todoList) {
+      if (item.text == todo) {
+        return alert("You've already had this ToDo!");
+      }
+    }
+    setTodoList([{text: todo, id: todoId}, ...todoList]);
   };
-  //Delete todo*****
-  const pressHandler = (key) => {
-    setTodoList((prevTodoList) => {
-      return prevTodoList.filter((todo) => todo.key != key);
-    });
+
+  const getInput = (text) => {
+    setTodo(text);
   };
-  //submit Todo****
-  const submitHandler = (todo) => {
-    todo.length &&
-      setTodoList((prevTodoList) => {
-        return [
-          {
-            text: todo,
-            key: Math.random().toString(),
-            check: <Icon name="check" size={25} color="#819ca9" />,
-            del: <Icon name="trash" size={30} color="#819ca9" />,
-          },
-          ...prevTodoList,
-        ];
-      });
-  };
+
+  const deleteTodo = (currentTodosId) =>
+    setTodoList(todoList.filter((todo) => todo.id !== currentTodosId));
+
   return (
-    <KeyboardAvoidingView
-      style={{flex: 1, backgroundColor: '#e0f2f1'}}
-      behavior={Platform.OS == 'android' ? null : 'padding:10'}>
-      <ScrollView style={{flex: 1}} bounces={false}>
-        <View style={{flex: 1}}>
-          <Header />
-          <AddTodo submitHandler={submitHandler} />
-          <View style={styles.container}>
-            <FlatList
-              data={todoList}
-              extraData={true}
-              renderItem={({item}) => (
-                <TodoItem
-                  item={item}
-                  pressHandler={pressHandler}
-                  pressCheck={pressCheck}
-                />
-              )}
-              keyExtractor={(item, index) => index.toString()}
-            />
-          </View>
-          <View style={styles.listReport}>
-            <Text style={[styles.text, {backgroundColor: '#80deea'}]}>
-              Done: {todoList.filter((todo) => todo.done).length}
-            </Text>
-            <Text
-              style={[
-                styles.text,
-                {backgroundColor: '#546e7a'},
-                {color: 'white'},
-              ]}>
-              Not Done:
-              {todoList.length - todoList.filter((todo) => todo.done).length}
-            </Text>
-          </View>
-          {/* <AddTodo submitHandler={submitHandler} /> */}
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.heading}>TODO</Text>
+        <Text style={styles.counter}>{todoList.length}</Text>
+      </View>
+
+      <View style={styles.main}>
+        <FlatList
+          keyExtractor={(item) => item.id.toString()}
+          data={todoList}
+          renderItem={({item}) => (
+            <ToDoCard content={item} deleteTodo={deleteTodo} />
+          )}
+        />
+      </View>
+
+      <View style={styles.footer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Type here to add your ToDo!"
+          onChangeText={getInput}
+          onSubmitEditing={(event) => {
+            setTodo(event.nativeEvent.text);
+            addTodo();
+            setId(todoId + 1);
+            setTodo('');
+          }}
+          autoFocus={true}
+          value={todo}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            addTodo();
+            setId(todoId + 1);
+            setTodo('');
+          }}>
+          <Text style={styles.btnText}>ADD TODO</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
-export default App;
-
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignContent: 'center',
-    // alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#37474F',
   },
-  listReport: {
+  header: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    margin: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 15,
+    marginVertical: 10,
   },
-  text: {
+  heading: {
+    color: '#FBA730',
     fontWeight: 'bold',
-    fontSize: 18,
-    backgroundColor: '#cbc26d',
-    padding: 10,
+    fontSize: 35,
+  },
+  counter: {
+    color: '#FBA730',
+    fontSize: 30,
+  },
+
+  main: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  footer: {
+    backgroundColor: '#B0BEC5',
+    marginHorizontal: 10,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: Dimensions.get('window').height / 5,
+    marginBottom: 25,
+  },
+  input: {
+    backgroundColor: '#FFFFFF',
+    width: '85%',
+    height: '32%',
     borderRadius: 10,
-    margin: 10,
+    marginVertical: 10,
+  },
+  button: {
+    backgroundColor: '#37474F',
+    width: '50%',
+    height: '30%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  btnText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
   },
 });
+
+export default App;
