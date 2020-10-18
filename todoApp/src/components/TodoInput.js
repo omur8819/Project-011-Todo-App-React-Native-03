d259f25fefe8e084b4ae585521fbb284fa4328d5
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-18 16:42:33 +0300

    project-update

---
+++ todoApp/src/components/TodoInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+
+const TodoInput = (props) => {
+    return (
+        <View>
+            <Text>Todo Input</Text>
+        </View>
+
+    
+)}
+
+export default TodoInput;
--- todoApp/src/styles.js
+++ todoApp/src/styles.js
@@ -6,13 +6,20 @@ const main = StyleSheet.create({
         backgroundColor: '#303e45'
     },
     banner:{
-        backgroundColor: 'red',
         flexDirection: 'row',
         justifyContent: 'space-between',
         padding: 10,
+        alignItems: "center",
     },
-    todoText: {},
-    todoCount: {},
+    todoText: {
+        fontSize: 50,
+        fontWeight: "bold",
+        color: '#ff9b31',
+    },
+    todoCount: {
+        fontSize: 35,
+        color: '#ff9b31',
+    },
 })
 
 export default main;
