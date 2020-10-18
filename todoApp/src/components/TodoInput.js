e2f94bd9fb35db2d7fc58c79da433c0b0e023614
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-18 17:55:16 +0300

    project-update

--- todoApp/src/components/TodoInput.js
+++ todoApp/src/components/TodoInput.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
+import { todo_input } from '../styles'
 
 const TodoInput = (props) => {
-    return (
-        <View>
-            <Text>Todo Input</Text>
-        </View>
+    const [text, setText] = useState("");
 
-    
-)}
+    return (
+        <View style={todo_input.container}>
+            <View style={todo_input.inputContainer}>
+                <TextInput
+                    placeholder="Type something to do.."
+                    onChangeText={value => setText(value)}
+                />
+            </View>
+
+            <TouchableOpacity
+                style={todo_input.buttonContainer}
+                onPress={() => props.onTodoEnter(text)}
+            >
+                <Text style={todo_input.buttonText}>ADD TODO</Text>
+            </TouchableOpacity>
+
+        </View>
+    )
+}
 
 export default TodoInput;
--- todoApp/src/styles.js
+++ todoApp/src/styles.js
@@ -1,25 +1,60 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+/**
+ * #303e45
+ * #a7b6bd
+ * #4a636e
+ * #ff9b31
+ */
 
 const main = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         backgroundColor: '#303e45'
     },
-    banner:{
+    banner: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         padding: 10,
-        alignItems: "center",
+        alignItems: 'center'
     },
     todoText: {
         fontSize: 50,
-        fontWeight: "bold",
-        color: '#ff9b31',
+        fontWeight: 'bold',
+        color: "#ff9b31"
     },
     todoCount: {
         fontSize: 35,
-        color: '#ff9b31',
-    },
+        color: "#ff9b31"
+    }
 })
 
-export default main;
+const todo_input = StyleSheet.create({
+    inputContainer: {
+        backgroundColor: '#e0e0e0',
+        padding: 10,
+        margin: 10,
+        borderRadius: 10
+    },
+    container: {
+        backgroundColor: '#a7b6bd',
+        padding: 10,
+        margin: 10,
+        borderRadius: 5
+    },
+    buttonContainer: {
+        backgroundColor: '#4a636e',
+        padding: 10,
+        margin: 10,
+        width: Dimensions.get('window').width / 2.5,
+        alignSelf: 'center',
+        borderRadius: 8
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
+
+export { main, todo_input };
