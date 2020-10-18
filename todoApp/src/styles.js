95b2e4ca2a1b10a4ca6e232c2beb8bee83803238
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-18 16:28:30 +0300

    project-update

---
+++ todoApp/src/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const main = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#303e45'
+    },
+    banner:{
+        backgroundColor: 'red',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+    todoText: {},
+    todoCount: {},
+})
+
+export default main;
